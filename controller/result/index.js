eec635745164cd060e95ad33571b75d4737faed2
darkstellar16 <itsmeshubham2799@gmail.com>
2023-05-02 00:33:10 +0530

    added all the remaining functionalities in controller

--- controller/result/index.js
+++ controller/result/index.js
@@ -1,2 +1,18 @@
-const quiz = require("../../models/quizModel.js");
+const Quiz = require("../../models/quizModel.js");
 
+const resultQuizzes = async (req, res) => {
+    try {
+        const quiz = await Quiz.findById(req.params.id);
+        if (quiz) {
+            const result = { rightAnswer: quiz.rightAnswer };
+            res.json(result);
+        } else {
+            res.status(404).json({ message: 'Quiz not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+
+module.exports = resultQuizzes;
