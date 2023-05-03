ee54bdc1a4abc002b249b6f01b749ea054ae193b
darkstellar16 <itsmeshubham2799@gmail.com>
2023-05-03 18:37:05 +0530

    added the status check feature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config;     //for environment variables..
-
+const cron = require('node-cron');
 const express = require("express");         //required express
 const app = express();
 
@@ -17,8 +17,29 @@ const main = require("./routes/index.js")
 app.use('/', main);
 
 
-const port = process.env.PORT || 2000                //for host
 
+
+cron.schedule('* * * * *', () => {
+    const now = new Date();
+    Quiz.find({}, (err, quizzes) => {
+        if (err) {
+            console.log(`Error updating quiz status: ${err}`);
+            return;
+        }
+        quizzes.forEach((quiz) => {
+            if (now < quiz.startDate) {
+                quiz.status = 'inactive';
+            } else if (now <= quiz.endDate) {
+                quiz.status = 'active';
+            } else {
+                quiz.status = 'finished';
+            }
+            quiz.save();
+        });
+    });
+});
+
+const port = process.env.PORT || 2000                //for host
 
 app.listen(port, () => {
     console.log(`Server is up and Running and the port is ${port}`);
